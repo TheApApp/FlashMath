809f8fa0973ace0f28ebcdfc45fb265041a01f66
Michael Rowe <michaelrowe01@gmail.com>
2023-11-18 12:07:52 -0500

    Initial Commit

---
+++ FlashMath/FlashMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMathApp.swift
+//  FlashMath
+//
+//  Created by Michael Rowe1 on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
